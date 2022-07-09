bf0de3bba1bc36b55b9aa9f33652aa79f00a6a38
Slava Markeyev <slava@nextdoor.com>
2022-07-08 18:19:58 -0700

    fix imports

--- src/sinks/http.rs
+++ src/sinks/http.rs
@@ -14,10 +14,7 @@ use http::{
 };
 use hyper::Body;
 
-use hyper::{
-    client,
-    client::{Client},
-};
+use hyper::{client, client::Client};
 
 use indexmap::IndexMap;
 use serde::{Deserialize, Serialize};
